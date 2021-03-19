592c9b0c40df39322b83e94a157f09ebb7e626ba
Israel <ishisra14@hotmail.com>
2021-03-18 19:59:06 -0600

    Sprites de los fondos del nivel 1 y 2, botones de jugabilidad y personaje 1.

--- core/src/mx/tec/astral/flytomars/PantallaNvl1.java
+++ core/src/mx/tec/astral/flytomars/PantallaNvl1.java
@@ -1,21 +1,97 @@
 package mx.tec.astral.flytomars;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 
 public class PantallaNvl1 extends Pantalla {
     private Juego juego;
+    Texture texturaFondo;
+    private Stage escenaMenuNiveles;
+    private Sprite hero;
+
     public PantallaNvl1(Juego juego) {
         this.juego = juego;
     }
 
     @Override
     public void show() {
+        crearMenu();
+    }
+
+    private void crearMenu() {
+        texturaFondo = new Texture("nivel1/lvlEarth.png");
+        hero = new Sprite(new Texture("nivel1/character1.png"));
+
+
+        // MENU, necesitamos una escena
+        //Escena
+        escenaMenuNiveles = new Stage(vista);
+
+        hero.setPosition(100, 100);
+
+        // Actores->Boton
+
+        Button btnA = crearBoton("buttons/btn_A.png", "buttons/btn_A_press.png");
+        Button btnB = crearBoton("buttons/btn_B.png", "buttons/btn_B_press.png");
+        //Button btnNvl3 = crearBoton("MenuNiveles/mars.png", "MenuNiveles/mars_press.png");
+
+
+        btnA.setPosition(ANCHO-400, 100, Align.center);
+        btnB.setPosition(ANCHO-200, 100, Align.center);
+
+        // Agrega los botones a escena
+        escenaMenuNiveles.addActor(btnA);
+        escenaMenuNiveles.addActor(btnB);
+
+        btnA.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                //juego.setScreen(new PantallaNvl1(juego));
+            }
+        });
+
+        btnB.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                //juego.setScreen(new PantallaNvl2(juego));
+            }
+        });
+
+
+        // La ESCENA se encarga de ATENDER LOS EVENTOS DE ENTRADA
+        Gdx.input.setInputProcessor(escenaMenuNiveles);
 
     }
 
+    private Button crearBoton(String archivo, String clickeado) {
+        Texture texturaBoton = new Texture(archivo);
+        TextureRegionDrawable trdBtn = new TextureRegionDrawable(texturaBoton);
+        // Clickeado
+        Texture texturaClickeada = new Texture(clickeado);
+        TextureRegionDrawable trdBtnClick = new TextureRegionDrawable(texturaClickeada);
+        return new Button(trdBtn, trdBtnClick);
+    }
     @Override
     public void render(float delta) {
-        borrarPantalla(1,0,1);
+
+        batch.setProjectionMatrix(camara.combined);
+
+        batch.begin();
+
+        batch.draw(texturaFondo, 0, 0);
+        //hero.draw(batch);
+
+        batch.end();
+
+        escenaMenuNiveles.draw();
     }
 
     @Override
