8028fb768018708e60268f5e7d68966fa5888876
AQJMTZ <aqjmtz@outlook.com>
2021-03-19 12:57:32 -0600

    Se agrego fonod a la pantalla de Acerca de

--- core/src/mx/tec/astral/flytomars/PantallaAcercaDe.java
+++ core/src/mx/tec/astral/flytomars/PantallaAcercaDe.java
@@ -1,12 +1,22 @@
 package mx.tec.astral.flytomars;
 
 import com.badlogic.gdx.Screen;
-
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+/*
+Autor Alejandro Quintana
+Fly to Mars V 1.0
+ */
 public class PantallaAcercaDe extends Pantalla {
+    private SpriteBatch batch;
+    private Texture texturaFondo;
     private Juego juego;
 
-    public PantallaAcercaDe(Juego juego) {
+    public PantallaAcercaDe(Juego juego)
+    {
         this.juego = juego;
+        this.texturaFondo = new Texture("menu/FondoMenu.png");
+        this.batch = new SpriteBatch();
     }
 
     @Override
@@ -18,6 +28,10 @@ public class PantallaAcercaDe extends Pantalla {
     public void render(float delta)
     {
         borrarPantalla(0,0,0);
+        batch.begin();
+        batch.draw(texturaFondo,0,0);
+        batch.end();
+
 
     }
 
