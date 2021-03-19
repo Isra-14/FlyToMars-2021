c21f6d41d95e942f1b955c388f76d824e4219fc0
Israel <ishisra14@hotmail.com>
2021-03-19 01:36:25 -0600

    Cambio de los fondos de pantallas adicionales.

--- core/src/mx/tec/astral/flytomars/PantallaAcercaDe.java
+++ core/src/mx/tec/astral/flytomars/PantallaAcercaDe.java
@@ -1,38 +1,77 @@
 package mx.tec.astral.flytomars;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-/*
-Autor Alejandro Quintana
-Fly to Mars V 1.0
- */
-public class PantallaAcercaDe extends Pantalla {
-    private SpriteBatch batch;
-    private Texture texturaFondo;
-    private Juego juego;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 
-    public PantallaAcercaDe(Juego juego)
-    {
+public class PantallaAcercaDe extends Pantalla {
+    private Juego juego;
+    Texture texturaFondo;
+    private Stage escenaMenuNiveles;
+
+    public PantallaAcercaDe(Juego juego) {
         this.juego = juego;
-        this.texturaFondo = new Texture("menu/FondoMenu.png");
-        this.batch = new SpriteBatch();
     }
 
     @Override
     public void show() {
+        crearMenu();
+    }
+
+    private void crearMenu() {
+        texturaFondo = new Texture("fondos/fondoAcerca.jpg");
+
+        // MENU, necesitamos una escena
+        //Escena
+        escenaMenuNiveles = new Stage(vista);
+
+        // Actores->Boton
+        Button btnBack = crearBoton("Menu/btn_back.png", "Menu/btn_back_press.png");
+
+        btnBack.setPosition(ANCHO/2, 200, Align.center);
+
+        // Agrega los botones a escena
+        escenaMenuNiveles.addActor(btnBack);
+
+        btnBack.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                juego.setScreen(new PantallaMenu(juego));
+            }
+        });
+
+
+        // La ESCENA se encarga de ATENDER LOS EVENTOS DE ENTRADA
+        Gdx.input.setInputProcessor(escenaMenuNiveles);
 
     }
 
+    private Button crearBoton(String archivo, String clickeado) {
+        Texture texturaBoton = new Texture(archivo);
+        TextureRegionDrawable trdBtn = new TextureRegionDrawable(texturaBoton);
+        // Clickeado
+        Texture texturaClickeada = new Texture(clickeado);
+        TextureRegionDrawable trdBtnClick = new TextureRegionDrawable(texturaClickeada);
+        return new Button(trdBtn, trdBtnClick);
+    }
+
     @Override
-    public void render(float delta)
-    {
-        borrarPantalla(0,0,0);
+    public void render(float delta) {
+        batch.setProjectionMatrix(camara.combined);
+
         batch.begin();
-        batch.draw(texturaFondo,0,0);
+
+        batch.draw(texturaFondo, 0, 0);
+
         batch.end();
 
-
+        escenaMenuNiveles.draw();
     }
 
     @Override
