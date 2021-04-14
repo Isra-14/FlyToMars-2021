1dac3d47ae75a3d8278b3db56f95bd1bfe40c96d
AQJMTZ <aqjmtz@outlook.com>
2021-04-14 15:56:05 -0600

    Se empezo a implementar la pantalla de GameOver

--- core/src/mx/tec/astral/flytomars/PantallaPreferencias.java
+++ core/src/mx/tec/astral/flytomars/PantallaPreferencias.java
@@ -7,12 +7,12 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 /*
 Autor Alejandro Quintana
  */
-public class PantallaOpciones extends Pantalla
+public class PantallaPreferencias extends Pantalla
 {
     private Juego juego;
     Texture texturaFondo;
     private Stage escenaMenuNiveles;
-    private static final String opcionVolumenMusica = "volume";
+    private static final String opcionVolumenMusica = "volumen";
     private static final String opcionMusicaActiva = "musica activada";
     private static final String opcionesSonido = "Sonido activado";
     private static final String volumenSonido = "Sonido";
@@ -45,7 +45,48 @@ public class PantallaOpciones extends Pantalla
         getOpciones().putFloat(opcionVolumenMusica, volumen);
         getOpciones().flush();
     }
-    
+
+    //Checa si esta activa la musica
+    public boolean musicaActivada()
+    {
+        return getOpciones().getBoolean(opcionMusicaActiva, true);
+    }
+
+    //Activa la musica o la desactiva
+   /* public boolean setMusicaActivada(boolean musicaActivada)
+    {
+        getOpciones().putBoolean(opcionMusicaActiva, musicaActivada);
+        getOpciones().flush();
+    }
+    */
+
+    //Checa el volumen de los sonidos
+    public float getVolumenSonido()
+    {
+        return getOpciones().getFloat(volumenSonido, .5f);
+    }
+
+    //Cambia el vvolumen del sonido
+    public void setVolumenSonido(float volumen)
+    {
+        getOpciones().putFloat(volumenSonido, volumen);
+        getOpciones().flush();
+    }
+
+    //Checa que el sonido este activado
+    public boolean sonidoActivado()
+    {
+        return getOpciones().getBoolean(opcionesSonido, true);
+    }
+
+    //Activa el sonido o lo desactiva
+    public void setSonidoActivado(boolean sonidoActivo)
+    {
+        getOpciones().putBoolean(opcionesSonido, sonidoActivo);
+        getOpciones().flush();
+
+    }
+
 
     @Override
     public void render(float delta) {
