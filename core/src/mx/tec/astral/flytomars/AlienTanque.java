b3f9c2c5a49a32fd4d7ec3f25eefeaa5bf95f95d
Israel <ishisra14@hotmail.com>
2021-04-27 01:19:13 -0500

    Se eliminan clases duplicadas

--- core/src/mx/tec/astral/flytomars/AlienTanque.java
+++
@@ -1,9 +0,0 @@
-package mx.tec.astral.flytomars;
-
-import com.badlogic.gdx.graphics.Texture;
-
-public class AlienTanque extends Objeto{
-    public AlienTanque(Texture textura, float x, float y){
-        super(textura, x, y);
-    }
-}
