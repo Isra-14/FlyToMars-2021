fc3b114b06124f564c63fd35ddb300c8a59fab3f
Israel <ishisra14@hotmail.com>
2021-04-27 01:33:07 -0500

    Pantalla Instrucciones de Misael Delgado

--- core/src/mx/tec/astral/flytomars/Pantallas/PantallaInstrucciones.java
+++ core/src/mx/tec/astral/flytomars/Pantallas/PantallaInstrucciones.java
@@ -14,12 +14,19 @@ import mx.tec.astral.flytomars.Texto;
 
 /*
     This class manage the Instructions screen.
-    Author: Israel Sanchez Hinojosa y Alejandro Quintana
+    Author: Misael Delgado, Israel Sanchez Hinojosa y Alejandro Quintana
 */
 
 public class PantallaInstrucciones extends Pantalla {
     private Juego juego;
     Texture texturaFondo;
+    Texture btnLeft;
+    Texture btnRigth;
+    Texture btnJump;
+    Texture btnShoot;
+    Texture coin;
+    Texture heart;
+
     private Stage escenaMenuNiveles;
     Texto texto;
     public PantallaInstrucciones(Juego juego) {
@@ -34,7 +41,12 @@ public class PantallaInstrucciones extends Pantalla {
     private void crearMenu() {
         texto = new Texto();
         texturaFondo = new Texture("fondos/fondoInstrucciones.jpg");
-
+        btnLeft = new Texture("buttons/izquierda.png");
+        btnRigth = new Texture("buttons/derecha.png");
+        btnJump = new Texture("buttons/btn_A.png");
+        btnShoot = new Texture("buttons/btn_B.png");
+        coin = new Texture("items/coin.png");
+        heart = new Texture("items/heart.png");
         // MENU, necesitamos una escena
         //Escena
         escenaMenuNiveles = new Stage(vista);
@@ -74,11 +86,18 @@ public class PantallaInstrucciones extends Pantalla {
         batch.setProjectionMatrix(camara.combined);
 
         batch.begin();
-
         batch.draw(texturaFondo, 0, 0);
-
-        texto.mostrarMensaje(batch, "Instrucciones del juego", ANCHO/2, .85f*ALTO);
-
+        batch.draw(btnLeft, ANCHO/7, .69f*ALTO);
+        batch.draw(btnRigth, ANCHO/7+ 50, .69f*ALTO + 3);
+        batch.draw(btnJump, ANCHO/7, .60f*ALTO,34,43);
+        batch.draw(btnShoot, ANCHO/7 + 50, .60f*ALTO, 34, 43);
+        batch.draw(coin, ANCHO/7 + 20, .51f*ALTO,34,43);
+        batch.draw(heart, ANCHO/7 + 20, .42f*ALTO, 34, 43);
+        texto.mostrarMensaje(batch, "Instrucciones del juego", ANCHO/2, .95f*ALTO);
+        texto.mostrarMensaje(batch, "Camina de izquierda a derecha", ANCHO/2, .75f*ALTO);
+        texto.mostrarMensaje(batch, "A - > Brincar , B -> Disparar", ANCHO/2, .66f*ALTO);
+        texto.mostrarMensaje(batch, "Recoge para desbloquear mundos", ANCHO/2, .57f*ALTO);
+        texto.mostrarMensaje(batch, "Recoge para obtener vida extra", ANCHO/2, .49f*ALTO);
         batch.end();
 
         escenaMenuNiveles.draw();
